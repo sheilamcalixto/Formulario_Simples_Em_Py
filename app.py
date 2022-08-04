420f834b70447650fc8c8641025fabaabd161752
Sheila Calixto <99204813+sheilamcalixto@users.noreply.github.com>
2022-08-04 19:24:14 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+import PySimpleGUI as sg
+
+layout = [
+    [sg.Text('Usuário')],
+    [sg.Input(key='usuario')],
+    [sg.Text('Senha')],
+    [sg.Input(key='senha')],
+    [sg.Button('login')],
+    [sg.Text('', key='mensagem')],
+    
+
+
+]
+
+window = sg.Window('Login', layout=layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WINDOW_CLOSED:
+        break
+    elif event == 'login':
+        senha_correta = '123456'
+        usuario_correto = 'sheilamcalixto'
+        usuario = values['usuario']
+        senha = values['senha']
+        if senha == senha_correta and usuario == usuario_correto:
+            window['mensagem'].update('Login feito com sucesso')
+        else:
+            window['mensagem'].update('Senha ou usuário incorreto')
+
+
+
+
+
+
